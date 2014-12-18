8f405bc664b2dfbeff27db76fb4fe4946b8948c2
Michiel Dethmers <git@phplist.com>
2014-12-18 20:50:43 +0000

    remember mapping of function to extension and report on the extensions required

--- get-env-functions.php
+++ get-env-functions.php
@@ -4,10 +4,10 @@
  * @author Glen Scott <glen@glenscott.co.uk>
  */
 
-define( 'CONFIG_PATH', '/tmp/php-functions.inc' );
+require_once 'config.php';
 
 $extensions          = get_loaded_extensions();
-$extension_functions = array();
+$function_extensions = array();
 $all_functions       = array();
 
 foreach ( $extensions as $ext ) {
@@ -15,10 +15,13 @@ foreach ( $extensions as $ext ) {
 
     if ( $functions ) {
         $all_functions = array_merge( $all_functions, $functions );
+        foreach ( $functions as $function) {
+          $function_extensions[ $function ] = $ext;
+        }
     }
 }
 
 echo "Found " . count( $all_functions ) . " total functions in " . count( $extensions ) . " extensions available in PHP.\n";
 
-// write out config file
-file_put_contents( CONFIG_PATH, serialize( $all_functions ) );
+file_put_contents( FUNCTION_CACHE_PATH, serialize( $all_functions ) );
+file_put_contents( EXTENSION_CACHE_PATH, serialize( $function_extensions ) );
